99b98022323b5f9512561d0de3aeb2be54e70264
Akansha <111658205+Akansha1409@users.noreply.github.com>
2024-08-06 01:43:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const io = socketio(server);
 app.set("view engine", "ejs");
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Handle socket connections
 io.on("connection", (socket) => {
