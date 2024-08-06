58d57b8114ee6f10b4227594fdff2c59d7554fab
Akansha1409 <akanshakalyan1409@gmail.com>
2024-08-06 09:50:45 +0530

    commit

--- app.js
+++ app.js
@@ -1,49 +1,33 @@
-require('dotenv').config(); // Load environment variables from .env file
-
 const express = require("express");
-const app = express();
-const path = require("path");
-const http = require("http");
 const socketio = require("socket.io");
+const http = require("http");
+const path = require("path");
 
+const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-// Set view engine
 app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, "public")));
-
-// Handle socket connections
-io.on("connection", (socket) => {
-    console.log(`New connection: ${socket.id}`);
-
-    // Handle incoming location data
-    socket.on("send-location", (data) => {
-        console.log(`Received location from ${socket.id}:`, data);
-        io.emit("receive-location", {
-            id: socket.id,
-            latitude: data.latitude,
-            longitude: data.longitude
-        });
+io.on("connection", function (socket) {
+  console.log("connected");
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", {
+      id: socket.id,
+      ...data,
     });
-
-    // Handle disconnection
-    socket.on("disconnect", () => {
-        io.emit("user-disconnected", socket.id);
-        console.log(`User disconnected: ${socket.id}`);
-    });
+  });
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", socket.id);
+  });
 });
 
-// Handle the root route
-app.get("/", (req, res) => {
-    res.render("index");
+app.get("/", function (req, res) {
+  res.render("index");
 });
 
-const port = process.env.PORT || 3001;
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-
-
+server.listen(8000, () => {
+  console.log("Server Start");
 });
